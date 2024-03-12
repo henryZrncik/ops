d88b11e6fce7032346f03d08926728e3f63a6c10
hzrncik <hzrncik@redhat.com>
2024-03-12 20:44:38 +0100

    junit setup
    Signed-off-by: hzrncik <hzrncik@redhat.com>

---
+++ src/main/java/org/example/DummyUtilClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class DummyUtilClass {
+    public static void doOperationA(final int i){
+        System.out.println(i);
+    }
+
+    public static void doOperationB(final boolean b){
+
+    }
+}
